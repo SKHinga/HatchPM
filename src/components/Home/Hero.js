2e130604260823ab522076a16587235500491308
Samuel Hinga <samuelhinga200@gmail.com>
2022-07-04 14:59:54 +0300

    Home navbar responsive

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink} from 'react-router-dom';
 import Button from '@mui/material/Button';
 
 function Hero() {
+  const [toggl, setToggl] = useState(false);
 
   const handleNew = () => {
     console.log("now");
   }
+
+  const handleToggle = () => {
+    setToggl(!toggl)
+  }
+
+
   return (
     <div className='login h-screen'>
-        <div className='grid grid-cols-12'>
-          <div className=''></div>
-          <div className='col-span-10'>
+        <div className='grid grid-cols-10 md:grid-cols-12'>
+          <div className='hidden md:contents'><div></div></div>
+          <div className='col-span-10 md:col-span-10'>
             <div className='flex justify-between py-1'>
               <h1 className='orange font-bold text-2xl'>Hatch.</h1>
-              <div className='flex justify-between w-80 mt-1 gress'>
+              <div className='flex justify-between md:w-80 mt-11 md:mt-1 gress bar'>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/'>Features</NavLink>
                 <NavLink to='/'>About Us</NavLink>
                 <NavLink to='/'>Contact</NavLink>
               </div>
-              <NavLink to='signin'><Button variant='outlined' className='material-button text-end' onClick={handleNew}>Log In</Button></NavLink>
+              <NavLink to='signin' className={toggl?'activebar':'bar'}><Button variant='outlined' className='material-button text-end' onClick={handleNew}>Log In</Button></NavLink>
+              <div className='hamburger' onClick={handleToggle}>
+                <span className='barr'></span>
+                <span className='barr'></span>
+                <span className='barr'></span>
+              </div>
             </div>
           </div>
-          <div className=''></div>
+          <div className='hidden md:contents'><div></div></div>
         </div>
     </div>
   )
